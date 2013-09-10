b6a2d3e5c2889759a0ab988aa986ff6bfd9ad6ed
Sefa Kilic <sefakilic@gmail.com>
2013-09-10 18:38:32 -0400

    chapter 5; not complete yet.

---
+++ 05_oh_my_gawd_full_of_stars.scm
@@ -0,0 +1,201 @@
+(define atom?
+  (lambda (x)
+    (and (not (pair? x)) (not (null? x)))))
+
+
+(define rember*
+  (lambda (a l)
+    (cond
+     ((null? l) '())
+     ((atom? (car l))
+      (cond
+       ((eq? (car l) a) (rember* a (cdr l)))
+       (else (cons (car l) (rember* a (cdr l))))))
+     (else (cons (rember* a (car l)) (rember* a (cdr l)))))))
+
+(rember* 'cup
+         '((coffee) cup ((tea) cup) (and (hick)) cup))
+                                        ; ((coffee) ((tea)) (and (hick)))
+
+(rember* 'sauce
+         '(((tomato sauce)) ((bean) sauce) (and ((flying)) sauce)))
+                                        ; (((tomato)) ((bean)) (and ((flying))))
+
+(define lat?
+  (lambda (l)
+    (cond
+     ((null? l) #t)
+     ((atom? (car l)) (lat? (cdr l)))
+     (else #f))))
+
+(lat? '(((tomato sauce))
+        ((bean) sauce)
+        (and ((flying)) sauce)))  ; #f
+
+(atom? (car '(((tomato sauce))
+              ((bean) sauce)
+              (and ((flying)) sauce))))   ; #f
+
+
+(define insertR*
+  (lambda (new old l)
+    (cond
+     ((null? l) '())
+     ((atom? (car l))
+      (cond
+       ((eq? old (car l)) (cons old
+                                (cons new (insertR* new old (cdr l)))))
+       (else (cons (car l) (insertR* new old (cdr l))))))
+     (else (cons (insertR* new old (car l))
+                 (insertR* new old (cdr l)))))))
+
+))))
+
+
+(insertR* 'roast
+          'chuck
+          '((how much (wood))
+            could
+            ((a (wood) chuck))
+            (((chuck)))
+            (if (a) ((wood chuck)))
+            could chuck wood))
+; ((how much (wood)) could ((a (wood) chuck roast)) (((chuck roast))) (if (a) ((wood
+; chuck roast))) could chuck roast wood)
+
+; -------------------------------------
+; The first commandment (final version)
+
+; When recurring on a list of atoms, lat, ask two questions about it: (null? lat) and
+; else.
+;
+; When recurring on a number, n, ask two questions about it: (zero? n) and else.
+; When recurring on a list of S-expressions, l, ask three questions about it: (null?
+; l), (atom? (car l)), and else.
+; -------------------------------------
+
+; ------------------------------------
+; The fourth commandment (final version)
+
+; Always change at least one argument while recurring. When recurring on a list of
+; atoms, lat, use (cdr lat). When recurring on a number, n, use (sub1 n). And when
+; recurring on a list of S-expressions, l, use (car l) and (cdr l) if neither (null?
+; l) nor (atom? (car l)) are true.
+
+; It must be changed to be closer to termination. The changing argument must be
+; tested in the termination condition:
+; - when using cdr, test termination with null? and
+; - when using sub1, test termination with zero?.
+; ------------------------------------
+
+(define add1
+  (lambda (n)
+    (+ n 1)))
+
+(define occur*
+  (lambda (a l)
+    (cond
+     ((null? l) 0)
+     ((atom? (car l))
+      (cond
+       ((eq? (car l) a) (add1 (occur* a (cdr l))))
+       (else (occur* a (cdr l)))))
+     (else (+ (occur* a (car l)) (occur* a (cdr l)))))))
+
+(occur* 'banana
+        '((banana)
+          (split ((((banana ice)))
+                  (cream (banana))
+                  sherbet))
+          (banana)
+          (bread)
+          (banana brandy)))   ; 5
+
+(define subst*
+  (lambda (new old l)
+    (cond
+     ((null? l) '())
+     ((atom? (car l))
+      (cond
+       ((eq? (car l) old) (cons new (subst* new old (cdr l))))
+       (else (cons (car l) (subst* new old (cdr l))))))
+     (else (cons (subst* new old (car l))
+                 (subst* new old (cdr l)))))))
+
+(subst* 'orange
+        'banana
+        '((banana)
+          (split ((((banana ice)))
+                  (cream (banana))
+                  sherbet))
+          (banana)
+          (bread)
+          (banana brandy)))
+
+; returns ((orange)
+;          (split ((((orange ice)))
+;                  (cream (orange))
+;                  sherbet))
+;          (orange)
+;          (bread)
+;          (orange brandy))
+
+(define insertL*
+  (lambda (new old l)
+    (cond
+     ((null? l) '())
+     ((atom? (car l))
+      (cond
+       ((eq? (car l) old) (cons new
+                                (cons old (insertL* new old (cdr l)))))
+       (else (cons (car l) (insertL* new old (cdr l))))))
+     (else (cons (insertL* new old (car l))
+                 (insertL* new old (cdr l)))))))
+
+(insertL* 'pecker
+          'chuck
+          '((how much  (wood)) 
+            could 
+            ((a  (wood)  chuck)) 
+            (( (chuck))) 
+            (if (a)  ((wood  chuck))) 
+            could  chuck wood))
+
+; returns ((how much  (wood)) 
+;          could 
+;          ((a  (wood)  pecker chuck)) 
+;          (((pecker chuck))) 
+;          (if (a)  ((wood  pecker chuck))) 
+;          could  pecker chuck wood).
+
+(define member*
+  (lambda (a l)
+    (cond
+     ((null? l) #f)
+     ((atom? (car l)) (or (eq? a (car l)) (member* a (cdr l))))
+     (else (or (member* a (car l)) (member* a (cdr l)))))))
+
+(member* 'chips
+         '((potato) (chips ((with) fish) chips)))   ; #t
+(member* 'chips
+         '((potato) (chips ((with) fish) (chips)))) ; #t
+
+(define leftmost
+  (lambda (l)
+    (cond
+     ((atom? (car l)) (car l))
+     (else (leftmost (car l))))))
+
+(leftmost '((potato) (chips ((with) fish) (chips)))) ; potato
+(leftmost '(((hot) (tuna (and))) cheese))            ; hot
+;(leftmost '(((() four )) 17 (seventeen)))            ; no answer
+
+(and (atom? (car '(mozzarella pizza)))
+     (eq? (car '(mozzarella pizza)) 'pizza))             ; #f
+
+(and (atom? (car '((mozzarella mushroom) pizza)))
+     (eq? (car '((mozzarella mushroom) pizza) 'pizza)))  ; #f
+
+(and (atom? (car '(pizza (tastes good))))
+     (eq? (car '(pizza (tastes good))) 'pizza))          ; #t
+
